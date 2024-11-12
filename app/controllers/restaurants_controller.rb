a873dd10f59e7bf0238eefcf02820371f3f9634b
Margarida Toureiro <margaridatoureiro@gmail.com>
2024-11-12 12:31:27 +0000

    update fixed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,7 +21,6 @@ class RestaurantsController < ApplicationController
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant.id)
   end
